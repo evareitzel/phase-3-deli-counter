03b8fda876f809d29a74fdb4e5350fc2fde39066
Eva Reitzel <evareitzel@gmail.com>
2022-11-12 23:16:37 -0500

    commit tree c460564ff403a289026b58b9b64cc11f92e96b18

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+# # Write your code here.
+# require 'pry'
+# names_arr = []
+
+def line(names_arr)
+  if names_arr.length == 0 #.empty?
+    puts "The line is currently empty." 
+  elsif names_arr.length > 0
+    waiting_line = "The line is currently:"
+    names_arr.each.with_index(1) do |person, num| 
+      waiting_line << " #{num}. #{person}"
+    end
+    puts waiting_line
+  end 
+end
+
+def take_a_number(names_arr, person)
+  names_arr << person
+  puts "Welcome, #{person}. You are number #{names_arr.length} in line."
+end
+
+def now_serving names_arr
+  if names_arr.empty? 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{names_arr[0]}." #names_arr.first
+    names_arr.shift
+  end
+end
